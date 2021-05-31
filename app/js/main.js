db442c1a2747afad077f396a0157567999009b00
AlexandrShnipov <sshnipov@gmail.com>
2021-06-01 01:26:31 +0300

    added page-contact

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,14 @@ $('.rightside-menu__close').on('click' , function () {
     autoplay: true
  });
 
+ $('.contact-slider').slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+   dots: true,
+   arrows: false,
+    
+});
+
  var mixer = mixitup('.gallery__inner', {
     load:{
        filter: '.living'
